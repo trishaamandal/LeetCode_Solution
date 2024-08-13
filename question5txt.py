170d622d8cbe8ec4ba053b42c754984f64a79ce7
trisha mandal <154074997+trishaamandal@users.noreply.github.com>
2024-08-13 22:34:57 +0530

    Create question5txt.py

---
+++ question5txt.py
@@ -0,0 +1,51 @@
+(5)Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+
+
+ANSWER:
+def backtrack(start, path, target):
+            if target < 0:
+                return
+            if target == 0:
+                res.append(path)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                backtrack(i + 1, path + [candidates[i]], target - candidates[i])
+        
+        candidates.sort()
+        res = []
+        backtrack(0, [], target)
+        return res
