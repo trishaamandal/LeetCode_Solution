7e578fc25d75b1ee208fc2882fd9d36fd32a8234
trisha mandal <154074997+trishaamandal@users.noreply.github.com>
2024-08-15 23:48:36 +0530

    Update question7txt.py

--- question7txt.py
+++ question7txt.py
@@ -30,3 +30,26 @@ Constraints:
 
 1 <= bills.length <= 105
 bills[i] is either 5, 10, or 20.
+
+# ANSWER:
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten=0, 0
+        for bill in bills:
+            if bill ==5:
+                five +=1
+            elif bill == 10:
+                if not five:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
